42ee09584bea496d70cde2f07441b1f3b26f2494
Nicola De Filippo <nicola@nicoladefilippo.it>
2024-02-11 17:01:59 +0100

    Initial Commit

---
+++ DragDropGame/DragDropGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropGameApp.swift
+//  DragDropGame
+//
+//  Created by nicola de filippo on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DragDropGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
